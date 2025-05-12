3f6328faf06c13eba7664724a46171f36f149628
Owen <ojones_6tc@outlook.com>
2025-05-12 13:31:21 +0100

    Added accuracy visualisation

--- train.py
+++ train.py
@@ -1,24 +1,52 @@
 import assessment_utils as au
+import matplotlib.pyplot as plt
+import json
+from datetime import datetime
 import torch
 
 if __name__ == "__main__":
     # Printing the shape of the dataset allows you to know the number of input features
-    for X, y in au.train_dataloader:
-        print(f"Shape of X [N, C, H, W]: {X.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}")
-        break
+    # for X, y in au.train_dataloader:
+    #     print(f"Shape of X [N, C, H, W]: {X.shape}")
+    #     print(f"Shape of y: {y.shape} {y.dtype}")
+    #     break
 
     # Instantiate the model
-    model = au.Cifar_NN(au.input_features).to(au.device)
-    print(model)
+    model = au.Cifar_NN().to(au.device)
+    # print(model)
 
-    optimiser = torch.optim.SGD(model.parameters(), lr=au.lr)
+    optimiser = torch.optim.SGD(model.parameters(), lr=au.learning_rate)
 
     # Perform the training
+    accuracies = [0]
+    start_time = datetime.now()
     for t in range(au.epochs):
-        print(f"Epoch {t+1}\n----------------------------")
+        print(
+            f"\nEpoch {t+1:>2d} / {au.epochs:>2d}\n{"_"*28}")
+
+        epoch_start_time = datetime.now()
+
         au.train(au.train_dataloader, model, au.loss_fn, optimiser)
-        au.test(au.test_dataloader, model, au.loss_fn)
-    print("Done!")
+        correct = au.test(au.test_dataloader, model, au.loss_fn)
+        accuracies.append(100*correct)
+
+        epoch_end_time = datetime.now()
+
+        print(
+            f"Epoch completed in {(epoch_end_time - epoch_start_time)}. Elapsed: {(epoch_end_time - start_time)}")
+        print(f"Improvement of {(accuracies[-1] - accuracies[-2]):0.1f}")
+
+    end_time = datetime.now()
+    print(f"Done in {end_time - start_time}!")
 
     au.save_model_weights(model, au.model_save_path)
+
+    with open("history.txt", "a") as history_file:
+        history_file.write(f"{accuracies}\n")
+
+    # Plot results
+    plt.scatter(range(0, au.epochs+1), accuracies)
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy / %")
+    plt.ylim(0, 100)
+    plt.show()
